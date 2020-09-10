9e16116f846a7a2883c167711afdd3bf4bb3fbdc
Nita666 <miralissa6@yahoo.com>
2020-09-10 16:24:35 +0500

    add inner fields generation for object properties (#1)
    Co-authored-by: avoinova <Anita.Voinova@billing.ru>

--- src/main/java/org/ran/jsonschema/suppliers/ObjectDataSupplier.java
+++ src/main/java/org/ran/jsonschema/suppliers/ObjectDataSupplier.java
@@ -6,7 +6,9 @@ import org.everit.json.schema.Schema;
 import org.ran.jsonschema.SupplyResolver;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class ObjectDataSupplier extends AbstractDataSupplier<ObjectSchema, Map<String, Object>> {
@@ -17,8 +19,8 @@ public class ObjectDataSupplier extends AbstractDataSupplier<ObjectSchema, Map<S
         Map<String, Object> mapData = new HashMap<>();
 
         // todo add support for min and max properties
-
-        schema.getRequiredProperties().forEach(stream -> {
+        Set<String> properties = schema.getPropertySchemas().keySet();
+        properties.forEach(stream -> {
             Schema propertySchema = schema.getPropertySchemas().get(stream);
             Object generatedExample = SupplyResolver.resolve(propertySchema).forSchema(propertySchema);
             mapData.put(stream, generatedExample);
