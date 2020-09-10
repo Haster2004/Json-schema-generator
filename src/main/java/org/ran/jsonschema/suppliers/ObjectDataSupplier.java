a4e755c0952bd1befa0818d656b73b1592ea26c8
avoinova <Anita.Voinova@billing.ru>
2020-09-10 15:52:03 +0500

    add inner fields generation for object properties

--- src/main/java/org/ran/jsonschema/suppliers/ObjectDataSupplier.java
+++ src/main/java/org/ran/jsonschema/suppliers/ObjectDataSupplier.java
@@ -6,7 +6,9 @@ import org.everit.json.schema.Schema;
 import org.ran.jsonschema.SupplyResolver;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class ObjectDataSupplier extends AbstractDataSupplier<ObjectSchema, Map<String, Object>> {
@@ -15,8 +17,8 @@ public class ObjectDataSupplier extends AbstractDataSupplier<ObjectSchema, Map<S
     @Override
     public Map<String, Object> generateDataFromSchema(ObjectSchema schema) {
         Map<String, Object> mapData = new HashMap<>();
-
-        schema.getRequiredProperties().forEach(stream -> {
+        Set<String> properties = schema.getPropertySchemas().keySet();
+        properties.forEach(stream -> {
             Schema propertySchema = schema.getPropertySchemas().get(stream);
             Object generatedExample = SupplyResolver.resolve(propertySchema).forSchema(propertySchema);
             mapData.put(stream, generatedExample);
--- src/test/java/org/ran/jsonschema/suppliers/ObjectDataSupplierTest.java
+++ src/test/java/org/ran/jsonschema/suppliers/ObjectDataSupplierTest.java
@@ -1,5 +1,6 @@
 package org.ran.jsonschema.suppliers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
@@ -11,6 +12,8 @@ import org.ran.jsonschema.SchemaDataSupplier;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ObjectDataSupplierTest {
 
@@ -28,6 +31,22 @@ class ObjectDataSupplierTest {
             JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
             Schema schema = SchemaLoader.load(rawSchema);
             System.out.println(new SchemaDataSupplier().get(schema));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void generateFromSchemaWithInnerFields() {
+        try (InputStream inputStream = getClass().getResourceAsStream("schemaWithInnerFields.json")) {
+            JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
+            Schema schema = SchemaLoader.load(rawSchema);
+            Object generatedData = new SchemaDataSupplier().get(schema);
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(generatedData);
+            assertTrue(json.matches("\\{\"a\":\"[A-z]*\",\"field1\":\\{\"field12\":\\{\"field123\":" +
+                    "\\{\"field123int\":[0-9]*,\"field123str\":\"[A-z]*\"}}},\"c\":[0-9]*}"));
         } catch (IOException e) {
             e.printStackTrace();
         }
